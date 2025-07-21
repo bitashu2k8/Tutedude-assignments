4bd907ff9817438d563f5e3bc25b0033671a2e03
bitashu2k8 <bit.ashu2k8@gmail.com>
2025-07-21 22:36:50 +0530

    Add files via upload

---
+++ Assignment 2 Task 1.py
@@ -0,0 +1,6 @@
+# program to check if the number is even or odd
+n=int(input("Enter a number: "))
+if (n%2==1):
+    print(n," is an odd number")
+else:
+    print(n,'is an even number')
---
+++ Assignment 2 Task 2.py
@@ -0,0 +1,6 @@
+# program for sum of the integer from 1 to 50
+Total=0
+for i in range(1,51):
+    Total+=i
+
+print("The sum of numbers from 1 to 50 is: ",Total)
